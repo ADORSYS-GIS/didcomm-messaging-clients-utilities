fee0de44a4ecbf374fc8623d204a0d37be032513
Ingrid Kamga <Ingrid.Kamga@adorsys.com>
2024-10-24 11:51:23 +0100

    chore: update eslint configuration

--- eslint.config.js
+++
@@ -1,7 +0,0 @@
-// eslint.config.js
-export default [
-  {
-    files: ['**/*.ts', '**/*.tsx'], // This pattern matches TypeScript files.
-    // Other ESLint rules and settings go here.
-  },
-];
--- libs/did-resolver-lib/src/resolver.test.ts
+++ libs/did-resolver-lib/src/resolver.test.ts
@@ -9,6 +9,6 @@ describe('sum module', () => {
           'did:peer:2.Ez6LSqXj3dXG5zL9bd4rUB21kDg5K6mBb5nRVArPbcAU8mX6b.Vz6Mku5fqS5Gm9iZy9nZ67uL4gJYvMMV454tmXJerWHWvp7tc.SeyJhIjpbImRpZGNvbW0vdjIiXSwiaWQiOiIjZGlkY29tbSIsInMiOiJodHRwOi8vYWxpY2UtbWVkaWF0b3IuY29tIiwidCI6ImRtIn0',
         )
         .then((result) => console.log(result)),
-    ).not.toBeNull;
+    ).not.toBeNull();
   });
 });
