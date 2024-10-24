7b92a2a1387b7aef8141a785ad2d9a4f5e7f9ac2
Christian_Yemele <yemelechristian2@gmail.com>
2024-10-24 09:22:07 +0100

    feat(): huge code base refactoring

--- packages/clients/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-import './index.css';
-import React from 'react';
-
-createRoot(document.getElementById('root')!).render(
-  <h1>DIDCOmm Mediator Client</h1>,
-);
